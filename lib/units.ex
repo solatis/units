7cbeaf596b35097fabd1c1122f45801d91eb4d9a
Leon Mergen <leon@solatis.com>
2015-11-16 16:05:31 +0700

    Renames :single to :one and :bytes to :byte

--- lib/units.ex
+++ lib/units.ex
@@ -1,7 +1,7 @@
 defmodule Units do 
 
-  @index %{:bytes => [
-            {:single, 1024}, 
+  @index %{:byte => [
+            {:one,    1024}, 
             {:kilo,   1024},
             {:mega,   1024},  
             {:giga,   1024},  
@@ -9,22 +9,22 @@ defmodule Units do
             {:peta,   1024}]}
 
   # Creates new unit
-  def from(amount, quantity), do: from(amount, :single, quantity)
-  def from(amount, unit, :bytes) when is_atom(unit) and is_integer(amount), do: %{amount: amount, unit: unit}
+  def from(amount, quantity), do: from(amount, :one, quantity)
+  def from(amount, unit, quantity) when is_atom(unit) and is_integer(amount), do: %{amount: amount, unit: unit, quantity: quantity}
 
   # Converts a unit to another and returns a unit
-  def to(amount, quantity), do: to(amount, :single, quantity)
-  def to(%{amount: amount, unit: src_unit}, dst_unit, quantity) when is_integer(amount) and is_atom(src_unit) and is_atom(dst_unit) and is_atom(quantity) do
-    index = @index[quantity]
-    %{amount: (convert index, amount, unit_index(index, src_unit), unit_index(index, dst_unit)), unit: dst_unit}
+  def to(amount, quantity), do: to(amount, :one, quantity)
+  def to(%{amount: amount, unit: src_unit, quantity: src_quantity}, dst_unit, dst_quantity) when is_integer(amount) and is_atom(src_unit) and is_atom(dst_unit) and is_atom(dst_quantity) and src_quantity == dst_quantity do
+    index = @index[dst_quantity]
+    %{amount: (convert index, amount, unit_index(index, src_unit), unit_index(index, dst_unit)), unit: dst_unit, quantity: dst_quantity}
   end
 
   # Converts a unit to another and returns an integer. Short for to/2 |> int/1
-  def to_int(amount, quantity), do: to_int(amount, :single, quantity)
+  def to_int(amount, quantity), do: to_int(amount, :one, quantity)
   def to_int(amount, dst_unit, quantity), do: to(amount, dst_unit, quantity) |> int
 
   # Converts a unit to another and returns a float. Short for to/2 |> float/1
-  def to_float(amount, quantity), do: to_float(amount, :single, quantity)
+  def to_float(amount, quantity), do: to_float(amount, :one, quantity)
   def to_float(amount, dst_unit, quantity), do: to(amount, dst_unit, quantity) |> float
 
   # Returns the amount as an integer
--- test/units_test.exs
+++ test/units_test.exs
@@ -4,58 +4,58 @@ defmodule UnitsTest do
 
   test "bytes" do
     # Regular bytes
-    assert (Units.from(1, :bytes)        |> Units.to_int(:bytes)) == 1
-    assert (Units.from(1, :kilo, :bytes) |> Units.to_int(:bytes)) == 1024
-    assert (Units.from(1, :mega, :bytes) |> Units.to_int(:bytes)) == (1024 * 1024)
-    assert (Units.from(1, :giga, :bytes) |> Units.to_int(:bytes)) == (1024 * 1024 * 1024)
-    assert (Units.from(1, :tera, :bytes) |> Units.to_int(:bytes)) == (1024 * 1024 * 1024 * 1024)
-    assert (Units.from(1, :peta, :bytes) |> Units.to_int(:bytes)) == (1024 * 1024 * 1024 * 1024 * 1024)
+    assert (Units.from(1, :byte)        |> Units.to_int(:byte)) == 1
+    assert (Units.from(1, :kilo, :byte) |> Units.to_int(:byte)) == 1024
+    assert (Units.from(1, :mega, :byte) |> Units.to_int(:byte)) == (1024 * 1024)
+    assert (Units.from(1, :giga, :byte) |> Units.to_int(:byte)) == (1024 * 1024 * 1024)
+    assert (Units.from(1, :tera, :byte) |> Units.to_int(:byte)) == (1024 * 1024 * 1024 * 1024)
+    assert (Units.from(1, :peta, :byte) |> Units.to_int(:byte)) == (1024 * 1024 * 1024 * 1024 * 1024)
 
     # Kilobytes 
-    assert (Units.from(1024, :bytes)        |> Units.to_int(:kilo, :bytes)) == 1
-    assert (Units.from(1,    :kilo, :bytes) |> Units.to_int(:kilo, :bytes)) == 1
-    assert (Units.from(1,    :mega, :bytes) |> Units.to_int(:kilo, :bytes)) == 1024
-    assert (Units.from(1,    :giga, :bytes) |> Units.to_int(:kilo, :bytes)) == (1024 * 1024)
-    assert (Units.from(1,    :tera, :bytes) |> Units.to_int(:kilo, :bytes)) == (1024 * 1024 * 1024)
-    assert (Units.from(1,    :peta, :bytes) |> Units.to_int(:kilo, :bytes)) == (1024 * 1024 * 1024 * 1024)
+    assert (Units.from(1024, :byte)        |> Units.to_int(:kilo, :byte)) == 1
+    assert (Units.from(1,    :kilo, :byte) |> Units.to_int(:kilo, :byte)) == 1
+    assert (Units.from(1,    :mega, :byte) |> Units.to_int(:kilo, :byte)) == 1024
+    assert (Units.from(1,    :giga, :byte) |> Units.to_int(:kilo, :byte)) == (1024 * 1024)
+    assert (Units.from(1,    :tera, :byte) |> Units.to_int(:kilo, :byte)) == (1024 * 1024 * 1024)
+    assert (Units.from(1,    :peta, :byte) |> Units.to_int(:kilo, :byte)) == (1024 * 1024 * 1024 * 1024)
 
     # Megabytes
-    assert (Units.from(1024 * 1024, :bytes)        |> Units.to_int(:mega, :bytes)) == 1
-    assert (Units.from(1024,        :kilo, :bytes) |> Units.to_int(:mega, :bytes)) == 1
-    assert (Units.from(1,           :mega, :bytes) |> Units.to_int(:mega, :bytes)) == 1
-    assert (Units.from(1,           :giga, :bytes) |> Units.to_int(:mega, :bytes)) == 1024
-    assert (Units.from(1,           :tera, :bytes) |> Units.to_int(:mega, :bytes)) == (1024 * 1024)
-    assert (Units.from(1,           :peta, :bytes) |> Units.to_int(:mega, :bytes)) == (1024 * 1024 * 1024)
+    assert (Units.from(1024 * 1024, :byte)        |> Units.to_int(:mega, :byte)) == 1
+    assert (Units.from(1024,        :kilo, :byte) |> Units.to_int(:mega, :byte)) == 1
+    assert (Units.from(1,           :mega, :byte) |> Units.to_int(:mega, :byte)) == 1
+    assert (Units.from(1,           :giga, :byte) |> Units.to_int(:mega, :byte)) == 1024
+    assert (Units.from(1,           :tera, :byte) |> Units.to_int(:mega, :byte)) == (1024 * 1024)
+    assert (Units.from(1,           :peta, :byte) |> Units.to_int(:mega, :byte)) == (1024 * 1024 * 1024)
 
     # Gigabytes
-    assert (Units.from(1024 * 1024 * 1024, :bytes)        |> Units.to_int(:giga, :bytes)) == 1
-    assert (Units.from(1024 * 1024,        :kilo, :bytes) |> Units.to_int(:giga, :bytes)) == 1
-    assert (Units.from(1024,               :mega, :bytes) |> Units.to_int(:giga, :bytes)) == 1
-    assert (Units.from(1,                  :giga, :bytes) |> Units.to_int(:giga, :bytes)) == 1
-    assert (Units.from(1,                  :tera, :bytes) |> Units.to_int(:giga, :bytes)) == 1024
-    assert (Units.from(1,                  :peta, :bytes) |> Units.to_int(:giga, :bytes)) == (1024 * 1024)
+    assert (Units.from(1024 * 1024 * 1024, :byte)        |> Units.to_int(:giga, :byte)) == 1
+    assert (Units.from(1024 * 1024,        :kilo, :byte) |> Units.to_int(:giga, :byte)) == 1
+    assert (Units.from(1024,               :mega, :byte) |> Units.to_int(:giga, :byte)) == 1
+    assert (Units.from(1,                  :giga, :byte) |> Units.to_int(:giga, :byte)) == 1
+    assert (Units.from(1,                  :tera, :byte) |> Units.to_int(:giga, :byte)) == 1024
+    assert (Units.from(1,                  :peta, :byte) |> Units.to_int(:giga, :byte)) == (1024 * 1024)
 
     # Terabytes
-    assert (Units.from(1024 * 1024 * 1024 * 1024, :bytes)        |> Units.to_int(:tera, :bytes)) == 1
-    assert (Units.from(1024 * 1024 * 1024,        :kilo, :bytes) |> Units.to_int(:tera, :bytes)) == 1
-    assert (Units.from(1024 * 1024,               :mega, :bytes) |> Units.to_int(:tera, :bytes)) == 1
-    assert (Units.from(1024,                      :giga, :bytes) |> Units.to_int(:tera, :bytes)) == 1
-    assert (Units.from(1,                         :tera, :bytes) |> Units.to_int(:tera, :bytes)) == 1
-    assert (Units.from(1,                         :peta, :bytes) |> Units.to_int(:tera, :bytes)) == (1024)
+    assert (Units.from(1024 * 1024 * 1024 * 1024, :byte)        |> Units.to_int(:tera, :byte)) == 1
+    assert (Units.from(1024 * 1024 * 1024,        :kilo, :byte) |> Units.to_int(:tera, :byte)) == 1
+    assert (Units.from(1024 * 1024,               :mega, :byte) |> Units.to_int(:tera, :byte)) == 1
+    assert (Units.from(1024,                      :giga, :byte) |> Units.to_int(:tera, :byte)) == 1
+    assert (Units.from(1,                         :tera, :byte) |> Units.to_int(:tera, :byte)) == 1
+    assert (Units.from(1,                         :peta, :byte) |> Units.to_int(:tera, :byte)) == (1024)
 
     # Petabytes
-    assert (Units.from(1024 * 1024 * 1024 * 1024 * 1024, :bytes)        |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1024 * 1024 * 1024 * 1024,        :kilo, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1024 * 1024 * 1024,               :mega, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1024 * 1024,                      :giga, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1024,                             :tera, :bytes) |> Units.to_int(:peta, :bytes)) == 1
+    assert (Units.from(1024 * 1024 * 1024 * 1024 * 1024, :byte)        |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1024 * 1024 * 1024 * 1024,        :kilo, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1024 * 1024 * 1024,               :mega, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1024 * 1024,                      :giga, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1024,                             :tera, :byte) |> Units.to_int(:peta, :byte)) == 1
 
     # Some tests that convert from petabyte to unit and back
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:bytes)        |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:kilo, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:mega, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:giga, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:tera, :bytes) |> Units.to_int(:peta, :bytes)) == 1
-    assert (Units.from(1, :peta, :bytes) |> Units.to(:peta, :bytes) |> Units.int)                   == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:byte)        |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:kilo, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:mega, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:giga, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:tera, :byte) |> Units.to_int(:peta, :byte)) == 1
+    assert (Units.from(1, :peta, :byte) |> Units.to(:peta, :byte) |> Units.int)                   == 1
   end
 end
